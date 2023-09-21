31e4602183180986dbe5b4776c484536c4c34a37
BT_1N3_04 <sample@bths.edu>
2023-09-21 10:17:22 -0400

    working on logic

--- src/fyouimgay.java
+++ src/fyouimgay.java
@@ -1,23 +1,30 @@
 
 import java.util.Scanner;
-public class fyouimgay {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("How many people in your group?: ");
-    int number = scan.nextInt();
-    int cost = 0;
-    int stringCompare = -1;
+public class fyouimgay{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many people in your group?: ");
+        int number = scan.nextInt();
+        System.out.print("Enter the tip percentage (0-100%): ");
+        int tip = scan.nextInt();
+        double cost = 0;
+        double totalCost = 0;
 
-    // the while loop condition is checked,
-    // and if TRUE, runs the code inside.
-    // when the code inside is done running, the condition is rechecked,
-    // and the loop repeats as long as the condition remains TRUE.
-    // when the condition becomes FALSE, it stops
-    while (cost == stringCompare) {
-        System.out.print("No silly, i said EVEN not odd! Try again: ");
-        number = scan.nextInt();
+        // the while loop condition is checked,
+        // and if TRUE, runs the code inside.
+        // when the code inside is done running, the condition is rechecked,
+        // and the loop repeats as long as the condition remains TRUE.
+        // when the condition becomes FALSE, it stops
+        while (cost != -1) {
+            System.out.print("Enter a cost in dollars and cents (type -1 to end): ");
+            cost = scan.nextDouble();
+            totalCost += cost;
+        }
+        totalCost++;
+        // code below the while loop runs after the loop ends
+        System.out.println("Total before tip: $" + totalCost);
+        System.out.println("Tip percentage: " + tip + "%");
+        System.out.print("Done!");
+
     }
-
-    // code below the while loop runs after the loop ends
-    System.out.print("Done!");
-
 }

639c302633433fd967768bd857dd5cfe41efb88e
BT_1N3_04 <sample@bths.edu>
2023-09-21 10:03:32 -0400

    working on logic

--- src/fyouimgay.java
+++ src/fyouimgay.java
@@ -1,14 +1,18 @@
+
+import java.util.Scanner;
 public class fyouimgay {
     Scanner scan = new Scanner(System.in);
     System.out.print("How many people in your group?: ");
     int number = scan.nextInt();
+    int cost = 0;
+    int stringCompare = -1;
 
     // the while loop condition is checked,
     // and if TRUE, runs the code inside.
     // when the code inside is done running, the condition is rechecked,
     // and the loop repeats as long as the condition remains TRUE.
     // when the condition becomes FALSE, it stops
-    while (number % 2 != 0) {
+    while (cost == stringCompare) {
         System.out.print("No silly, i said EVEN not odd! Try again: ");
         number = scan.nextInt();
     }

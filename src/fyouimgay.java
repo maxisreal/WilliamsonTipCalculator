dfda042745edbc4451e919dcb4a3a76c4429689e
max <maximillianw6@nycstudents.net>
2023-10-01 23:18:54 -0400

    final commit

--- src/fyouimgay.java
+++ src/fyouimgay.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 import java.text.DecimalFormat;
@@ -13,11 +14,12 @@ public class fyouimgay{
         System.out.print("Enter the tip percentage (0-100%): ");
         double tip = scan.nextDouble();
         double cost = 0;
-        while (tip <= 100){
+        while (tip > 100){
             System.out.print("Enter the tip percentage (0-100%): ");
             tip = scan.nextDouble();
         }
         double totalCost = 0;
+
 
         while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents (type -1 to end): ");
@@ -25,20 +27,45 @@ public class fyouimgay{
             totalCost += cost;
         }
         totalCost++; //gets rid of -1
-        //pushtest
+        String currency = null;
+        double convert = 2;
+        while (convert == 2) {
+            System.out.println("Enter currency type (₩, $, €, ¥): ");
+            currency = scan.nextLine();
+            switch (currency) {
+                case "yen" -> {
+                    currency = "¥";
+                    convert = 149.69;
+                }
+                case "won" -> {
+                    currency = "₩";
+                    convert = 1351;
+                }
+                case "dollar", "usd" -> {
+                    currency = "$";
+                    convert = 1;
+                }
+                case "euro" -> {
+                    currency = "€";
+                    convert = 0.95;
+                }
+                default -> currency = "?";
+            }
+        }
+            totalCost *= convert;
         System.out.println("___________________________________");
-        System.out.println("Total before tip: $" + df.format(totalCost));
+        System.out.println("Total before tip: " + currency + df.format(totalCost*convert));
         System.out.println("Tip percentage: " + (int)(tip) + "%");
         tip *=0.01;
-        System.out.println("Tip: $" + totalCost*tip);
+        System.out.println("Tip: "+ currency + df.format(totalCost*tip));
         tip++;
 
-        System.out.println("Total after tip: $" + df.format(totalCost*tip));
-        System.out.println("Total per person before tip: $" + df.format(totalCost/number));
+        System.out.println("Total after tip: " + currency + df.format(totalCost*tip));
+        System.out.println("Total per person before tip: " + currency + df.format(totalCost/number));
         tip--;
-        System.out.println("Tip per person: $" + df.format(totalCost*tip/number));
+        System.out.println("Tip per person: "+currency + df.format(totalCost*tip/number));
         tip++;
-        System.out.println("Total per person: $" + df.format(totalCost*tip/number));
+        System.out.println("Total per person: "+currency + df.format(totalCost*tip/number));
         System.out.print("Done!");
     }
 }
